2a69cca7240e860a0148be239120b4c76772f795
Sam <Sam@LAPTOP-8SIETBN0>
2019-02-15 00:14:39 +0800

    Add MemberDAO Class

---
+++ src/com/member/model/MemberDAO.java
@@ -0,0 +1,33 @@
+package com.member.model;
+
+import java.util.List;
+
+public class MemberDAO implements MemberDAO_interface {
+
+	@Override
+	public void insert(MemberVO memberVO) {
+		
+	}
+
+	@Override
+	public void update(MemberVO memberVO) {
+		
+	}
+
+	@Override
+	public void delete(MemberVO memberVO) {
+		
+	}
+
+	@Override
+	public MemberVO findByPrimaryKey(String memid) {
+		return null;
+		
+	}
+
+	@Override
+	public List<MemberVO> getAll() {
+		return null;
+	}
+
+}
---
+++ src/com/member/model/MemberDAO_interface.java
@@ -0,0 +1,17 @@
+package com.member.model;
+
+import java.util.List;
+
+public interface MemberDAO_interface {
+	
+	public void insert(MemberVO memberVO);
+	public void update(MemberVO memberVO);
+	public void delete(MemberVO memberVO);
+	public MemberVO findByPrimaryKey(String memid);
+	public List<MemberVO> getAll();
+	
+//	萬用複合查詢(傳入參數型別Map)(回傳List)
+	
+//	public List<MemberVO> getAll(Map<String, String> map);
+	
+}
