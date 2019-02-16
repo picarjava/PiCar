f51481685b46e136f4a6fba650b5de05900e0f73
Sam <Sam@LAPTOP-8SIETBN0>
2019-02-17 00:23:16 +0800

    JNDI測試OK

---
+++ src/com/member/controller/MemberServlet.java
@@ -0,0 +1,40 @@
+package com.member.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.member.model.*;
+
+/**
+ * Servlet implementation class MemberServlet
+ */
+
+public class MemberServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+		res.setContentType("text/html;charset=UTF-8");
+		String action = req.getParameter("action");
+		PrintWriter out = res.getWriter();
+		
+		MemberDAO memberDAO = new MemberDAO();
+		MemberVO memberVO = memberDAO.findByPrimaryKey(action);
+		System.out.println(memberVO.getCreditcard());
+		out.println(memberVO.getName());
+//		memberDAO.delete("ation");
+//		out.print("delete ok");
+		doGet(req, res);
+	}
+
+}
