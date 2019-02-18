b09e65057b6ebd663f86378b672420f78cf39b04
rui <superjungle007@gmail.com>
2019-02-18 18:19:10 +0800

    update broadcastjdbcdao 18:19

--- src/com/broadcast/model/BroadcastJDBCDAO.java
+++ src/com/broadcast/model/BroadcastJDBCDAO.java
@@ -8,7 +8,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.activity.model.ActivityVO;
 
 public class BroadcastJDBCDAO implements BroadcastDAO_interface {
 
