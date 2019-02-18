aa3b830c18a1e07e51be5ea51d77409f1a763ee3
rui <superjungle007@gmail.com>
2019-02-18 23:05:45 +0800

    driverDAO ing 2305

--- src/com/driver/model/DriverJDBCDAO.java
+++ src/com/driver/model/DriverJDBCDAO.java
@@ -18,12 +18,12 @@ public class DriverJDBCDAO implements DriverDAO_interface {
 			+ "SCORE, CAR_TYPE, SHARED_CAR, PET, SMOKE, BABY_SEAT) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 //	private static final String GET_ALL_STMT =
 //			"SELECT * FROM DRIVER ORDER BY DRIVER_ID";
-//	private static final String GET_ONE_STMT =
-//	"SELECT MEM_ID, DRIVER_ID, PLATE_NUM, LICENCE, CRIMINAL, TRAFFIC_RECORD, ID_NUM, PHOTO, VERIFIED, BANNED, DEADLINE, ONLINE_CAR, SCORE, CAR_TYPE, SHARED_CAR, PET, SMOKE, BABY_SEAT FROM DRIVER WHERE DRIVER_ID";
+	private static final String GET_ONE_STMT =
+	"SELECT MEM_ID, DRIVER_ID, PLATE_NUM, LICENCE, CRIMINAL, TRAFFIC_RECORD, ID_NUM, PHOTO, VERIFIED, BANNED, DEADLINE, ONLINE_CAR, SCORE, CAR_TYPE, SHARED_CAR, PET, SMOKE, BABY_SEAT FROM DRIVER WHERE DRIVER_ID";
 	private static final String DELETE = 
 	"DELETE FROM DRIVER where DRIVER_ID = ?";
-//	private static final String UPDATE = 
-//	"UPDATE DRIVER SET PLATE_NUM=?, LICENCE=?, CRIMINAL=?, TRAFFIC_RECORD=?, PHOTO=?, VERIFIED=?, BANNED=?, DEADLINE=?, ONLINE_CAR=?, SCORE=?, CAR_TYPE=?, SHARED_CAR=?, PET=?, SMOKE=?, BABY_SEAT=? where DRIVER=?";
+	private static final String UPDATE = 
+	"UPDATE DRIVER SET PLATE_NUM=?, LICENCE=?, CRIMINAL=?, TRAFFIC_RECORD=?, PHOTO=?, VERIFIED=?, BANNED=?, DEADLINE=?, ONLINE_CAR=?, SCORE=?, CAR_TYPE=?, SHARED_CAR=?, PET=?, SMOKE=?, BABY_SEAT=? where DRIVER=?";
 
 	@Override
 	public void insert(DriverVO driverVO) {
@@ -79,20 +79,63 @@ public class DriverJDBCDAO implements DriverDAO_interface {
 		}
 
 	}
-//    public static void main(String[] args) throws ClassNotFoundException {
-//  Class.forName("oracle.jdbc.driver.OracleDriver");
-//  DriverDAO_interface DriverJDBCDAO = new DriverJDBCDAO();
-//  System.out.println(DriverJDBCDAO.insert(null));
-//  
 
-//  DriverJDBCDAO driverVO = driverDAO.findByPrimaryKey("2");
-//  driverVO.setStartLoc("DR006");
-//  System.out.println(singleOrderDAO.findByPrimaryKey("2"));
-//  System.out.println(singleOrderDAO.getAll());
-//}
 
 	@Override
 	public void update(DriverVO driverVO) {
+		
+		Connection con =null;
+		PreparedStatement pstmt= null;
+		try{
+			Class.forName(driver);
+			con=DriverManager.getConnection(url,userid,passwd);
+			pstmt=con.prepareStatement(UPDATE);
+			/*根據SQL常數，用vo.getxxx去set pstmt需要的值*/
+			
+			pstmt.setString(1, driverVO.getMemID());
+			pstmt.setString(2, driverVO.getDriverID());
+			pstmt.setString(3, driverVO.getPlateNum());
+			pstmt.setBytes(4, driverVO.getLicence());
+			pstmt.setBytes(5, driverVO.getCriminal());
+			pstmt.setBytes(6, driverVO.getTrafficRecord());
+			pstmt.setBytes(7, driverVO.getIdNum());
+			pstmt.setBytes(8, driverVO.getPhoto());
+			pstmt.setInt(9, driverVO.getVerified());
+			pstmt.setInt(10, driverVO.getBanned());
+			pstmt.setDate(11, driverVO.getDeadline());
+			pstmt.setInt(12, driverVO.getOnlineCar());
+			pstmt.setInt(13, driverVO.getScore());
+			pstmt.setString(14, driverVO.getCarType());
+			pstmt.setInt(15, driverVO.getSharedCar());
+			pstmt.setInt(16, driverVO.getPet());
+			pstmt.setInt(17, driverVO.getSmoke());
+			pstmt.setInt(18, driverVO.getBabySeat());
+			
+			pstmt.executeUpdate();
+			
+		}catch(ClassNotFoundException e){
+			throw new RuntimeException("無法載入DB Driver"+e.getMessage());
+		}catch(SQLException se){
+			throw new RuntimeException ("發生SQL error"+se.getMessage());
+		}finally{
+			if(pstmt !=null){
+				try{
+					pstmt.close();
+				}catch(SQLException se){
+					se.printStackTrace(System.err);
+				}
+			}
+			if(con !=null){
+				try{
+					con.close();
+				}catch(Exception e){
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+	
+		
+		
 
 	}
 
@@ -130,8 +173,6 @@ public class DriverJDBCDAO implements DriverDAO_interface {
 				}
 			}
 		}
-	
-
 	}
 
 	@Override
@@ -145,3 +186,14 @@ public class DriverJDBCDAO implements DriverDAO_interface {
 	}
 
 }
+//public static void main(String[] args) throws ClassNotFoundException {
+//Class.forName("oracle.jdbc.driver.OracleDriver");
+//DriverDAO_interface DriverJDBCDAO = new DriverJDBCDAO();
+//System.out.println(DriverJDBCDAO.insert(null));
+//
+
+//DriverJDBCDAO driverVO = driverDAO.findByPrimaryKey("2");
+//driverVO.setStartLoc("DR006");
+//System.out.println(singleOrderDAO.findByPrimaryKey("2"));
+//System.out.println(singleOrderDAO.getAll());
+//}
