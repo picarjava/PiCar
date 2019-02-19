082e415393459c8de938cf7b9eb899085a0a388c
123 <123@123-PC>
2019-02-20 02:25:14 +0800

    updeta serclet 1/2 02/24 2019:2/20

---
+++ WebContent/front-end/groupBand/updateGroupBand.jsp
@@ -0,0 +1,295 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ page import="com.groupBand.model.*"%>
+
+<%
+	GroupBandVO groupBandVO = (GroupBandVO) request.getAttribute("groupBandVO");
+%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css"
+	href="../../datetimepicker/jquery.datetimepicker.css" />
+<script src="../../datetimepicker/jquery.js"></script>
+<script src="../../datetimepicker/jquery.datetimepicker.full.js"></script>
+<meta charset="BIG5">
+<title>Insert title here</title>
+<style>
+table#table-1 {
+	width: 450px;
+	background-color: #008888;
+	margin-top: 5px;
+	margin-bottom: 10px;
+	border: 3px ridge Gray;
+	height: 80px;
+	text-align: center;
+}
+
+table#table-1 h4 {
+	color: red;
+	display: block;
+	margin-bottom: 1px;
+}
+
+h4 {
+	color: blue;
+	display: inline;
+}
+</style>
+
+<style>
+table {
+	width: 450px;
+	background-color: white;
+	margin-top: 1px;
+	margin-bottom: 1px;
+}
+
+table, th, td {
+	border: 0px solid #CCCCFF;
+}
+
+th, td {
+	padding: 1px;
+}
+</style>
+</head>
+<body bgcolor=#aaaaaa>
+
+	<c:if test="${not empty errorMsgs}">
+		<font style="color: red">請修正以下錯誤:</font>
+		<ul>
+			<c:forEach var="message" items="${errorMsgs}">
+				<li style="color: red">${message}</li>
+			</c:forEach>
+		</ul>
+	</c:if>
+	<table id="table-1">
+
+
+		<tr>
+			<td>
+				<h3>發起揪團 - insertGroupBand.jsp</h3>
+			</td>
+			<td>
+				<h4>
+					<a href="select_page.jsp">回首頁</a>
+				</h4>
+			</td>
+		</tr>
+	</table>
+	<h3>發起揪團</h3>
+
+	<form action="/PiCar/GroupBand" method="POST" enctype="multipart/form-data">
+		<table>
+
+			<tr>
+				<td><input type="radio" name="orderT" value="0" checked
+					onclick="groupif(this.value)">揪團</td>
+				<td><input type="radio" name="orderT" value="1"
+					onclick="groupif(this.value)">長期揪團<br></td>
+			</tr>
+			
+			<tr>
+			<td>揪團類別</td>
+			<td><select name="groupType">
+			<option value="演唱會">演唱會</option>
+			<option value="旅遊">旅遊</option>
+			<option value="美食">美食</option>
+			<option value="運動團">運動團</option>
+			<option value="展覽">展覽</option>
+			<option value="遊樂園">遊樂園</option>
+			</select></td>
+			</tr>
+			
+
+			<tr>
+				<td>團名</td>
+				<td><input type="TEXT" name="groupName" size="25"
+					value="<%=(groupBandVO == null) ? "" : groupBandVO.getGroupName()%>" /></td>
+			</tr>
+			<tr>
+				<td>簡介:</td>
+
+				<td><textarea name="introduction" id="note" rows="3" cols="50"><%=(groupBandVO == null) ? "" : groupBandVO.getIntroduction()%> </textarea></td>
+			</tr>
+
+			<tr>
+				<td>揪團圖片</td>
+
+				<td><input type="file" name="photo" size="25"
+					value="<%=(groupBandVO == null) ? "" : groupBandVO.getPhoto()%>" />
+				</td>
+			</tr>
+
+			<tr>
+				<td>上車地點:</td>
+
+				<td><select name="startLoc">
+						<option value="桃園火車站">桃園市</option>
+						<option value="a02">台北市</option>
+				</select></td>
+			</tr>
+
+			<tr>
+				<td>下車地點:</td>
+
+				<td><select name="endLoc">
+						<option value="桃園火車站">桃園市</option>
+						<option value="台北市火車站">台北市</option>
+				</select></td>
+			</tr>
+
+			<tr>
+				<td>下限人數</td>
+				<td><select id="lowerLimit" onchange="pvalue(this.value);" name="lowerlimit">
+						<%
+							for (int a = 2; a < 5; a++) {
+						%>
+						<option name="<%=a%>" value="<%=a%>"><%=a%></option>
+						<%
+							}
+						%>
+				</select></td>
+			</tr>
+
+
+			<tr>
+				<td>上限人數</td>
+				<td><select id="Lower" name="upperlimit">
+						<%
+							for (int a = 2; a < 5; a++) {
+						%>
+						<option name="<%=a%>" value="<%=a%>"><%=a%></option>
+						<%
+							}
+						%>
+						<script language="Javascript">
+							function pvalue(s) {
+
+								Lower = document.getElementById("Lower");
+
+								Lower.innerHTML = "";
+
+								for (x = s; x < 5; x++) {
+									Lower.innerHTML = Lower.innerHTML
+											+ '<option name="'+x+'" value="'+x+'">'
+											+ x + '</option>';
+
+								}
+
+							}
+						</script></td>
+			</tr>
+
+
+			<tr>
+				<td>上車日期</td>
+
+				<td><input name="startTime" id="start_date" type="text"
+					style="display: none"> 
+					<input name="startTimes" id="f_date1"
+					type="text"></td>
+			</tr>
+
+			<tr>
+				<td><nobr id="enddate" style="display:none">結束日期:</nobr></td>
+
+				<td><input name="endtime" id="end_date" type="text"
+					style="display: none"></td>
+			</tr>
+
+
+			<td>備註:</td>
+
+			<td><textarea name="note" id="note" rows="3" cols="50"><%=(groupBandVO == null) ? "" : groupBandVO.getNote()%> </textarea></td>
+			</tr>
+			<tr>
+				<td>隱私設定:</td>
+
+				<td><input type="checkbox" value="1" name="privates">隱私權<br>
+				</td>
+			</tr>
+
+
+			<tr>
+				<td><input type="hidden" name="action" value="insert" /> <input
+					type="submit" value="發起揪團" /></td>
+				<td><input type="reset" value="清除揪團" /></td>
+
+			</tr>
+		</table>
+	</form>
+</body>
+
+<script>
+	$.datetimepicker.setLocale('zh'); // kr ko ja en
+	$('#f_date1').datetimepicker({
+		theme : '', //theme: 'dark',
+		timepicker : true, //timepicker: false,
+		step : 1, //step: 60 (這是timepicker的預設間隔60分鐘)
+		format : 'Y-m-d H:i:s',
+		value : new Date(),
+	//disabledDates:    ['2017/06/08','2017/06/09','2017/06/10'], // 去除特定不含
+	//startDate:	        '2017/07/10',  // 起始日
+	minDate:           '-1970-01-01', // 去除今日(不含)之前
+	maxDate:           '+1970-01-20'  // 去除今日(不含)之後
+	});
+</script>
+
+<script>
+	$.datetimepicker.setLocale('zh'); // kr ko ja en
+	$(function() {
+		$('#start_date').datetimepicker(
+				{
+					format : 'Y-m-d H:i:s',
+					onShow : function() {
+						this.setOptions({
+							maxDate : $('#end_date').val() ? $('#end_date')
+									.val() : false
+						})
+					},
+					timepicker : true,
+					
+					minDate:           '-1970-01-01', // 去除今日(不含)之前
+					maxDate:           '+1970-01-20'  // 去除今日(不含)之後
+				});
+
+		$('#end_date').datetimepicker(
+				{
+					format : 'Y-m-d H:i:s',
+					onShow : function() {
+						this.setOptions({
+							minDate : $('#start_date').val() ? $('#start_date')
+									.val() : false
+						})
+					},
+					timepicker : false,
+					minDate:           '-1970-01-01', // 去除今日(不含)之前
+					maxDate:           '+1970-01-20'  // 去除今日(不含)之後
+				});
+	});
+</script>
+
+<script>
+	function groupif(number) {
+		startdate = document.getElementById("start_date");
+		enddate = document.getElementById("end_date");
+		fdate1 = document.getElementById("f_date1");
+		enddates = document.getElementById("enddate");
+		if (number == 1) {
+			startdate.style = "";
+			enddate.style = "";
+			fdate1.style = "display:none";
+			enddates.style = "";
+		} else {
+			startdate.style = "display:none";
+			enddate.style = "display:none";
+			fdate1.style = "";
+			enddates.style = "display:none";
+
+		}
+	}
+</script>
+</html>
--- src/com/groupBand/controller/GroupBandServlet.java
+++ src/com/groupBand/controller/GroupBandServlet.java
@@ -52,7 +52,203 @@ public class GroupBandServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		req.setCharacterEncoding("UTF-8");
 		String action = req.getParameter("action");
+	   	
+		if ("getOne_For_Update".equals(action)) {
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+			try {
+			String groupID = req.getParameter("groupID");
+			
+			GroupBandService groupBandService = new GroupBandService();
+			GroupBandVO groupBandVO = groupBandService.getOneGroupBand(groupID);
+			
+			req.setAttribute("GroupBandVO",groupBandVO);
+			String url = "/front-end/groupBand/updateGroupBand.jsp";
+			RequestDispatcher successView = req.getRequestDispatcher(url);// ���\��� update_emp_input.jsp
+			successView.forward(req, res);
+			} catch (Exception e) {
+				errorMsgs.add("�L�k���o�n�ק諸���:" + e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/emp/listAllEmp.jsp");
+				failureView.forward(req, res);
+			}
+			
+		}
 		
+		if ("update".equals(action)) { 
+			List<String> errorMsgs = new LinkedList<String>();
+			// Store this set in the request scope, in case we need to
+			// send the ErrorPage view.
+			req.setAttribute("errorMsgs", errorMsgs);
+
+			try {
+				/***********************1.�����ШD�Ѽ� - ��J�榡�����~�B�z*************************/
+				
+				String content =" ";	
+				
+				
+				String introduction = req.getParameter("introduction");
+				String introduc = "^[(\\u4e00-\\u9fa5)(a-zA-Z0-9_)]{1,20}$";
+				if (introduction == null || introduction.trim().length() == 0) {
+					errorMsgs.add("簡介: 請勿空白");
+				} else if(!introduction.trim().matches(introduc)) { //以下練習正則(規)表示式(regular-expression)
+					errorMsgs.add("簡介: 只能是中、英文字母、數字和_ , 且長度必需在2到10之間");
+	            }
+				
+				
+				Integer groupStatus =1;
+				
+						
+				Integer currenTnum  =1;
+				
+				
+				Integer upperLimit = new Integer(req.getParameter("upperlimit").trim());
+				
+				
+				Integer lowerLimit = new Integer(req.getParameter("lowerlimit").trim());
+				
+				
+				String groupName =req.getParameter("groupName");
+				String groupN = "^[(\u4e00-\u9fa5)(a-zA-Z0-9_)]{1,20}$";
+				if (groupName == null || groupName.trim().length() == 0) {
+					errorMsgs.add("團名: 請勿空白");
+				} else if(!groupName.trim().matches(groupN)) { //以下練習正則(規)表示式(regular-expression)
+					errorMsgs.add("團名: 只能是中、英文字母、數字和_ , 且長度必需在2到10之間");
+	            }
+				
+				String groupLeader = "M001";
+				
+				
+				String startLoc = req.getParameter("startLoc");
+				
+				
+				String endLoc = req.getParameter("endLoc");
+				
+				
+				Integer privates =1;
+				if("1".equals(req.getParameter("privates"))) {
+				privates = new Integer(req.getParameter("privates").trim());	
+				}else {
+				privates =0;	
+					  }
+				
+				byte[] photo=null;
+				
+				Collection<Part> parts = req.getParts();
+				for (Part part : parts) {
+					if (getFileNameFromPart(part) != null && part.getContentType()!=null) {
+					
+						
+						long size = part.getSize();
+
+						// 額外測試 InputStream 與 byte[] (幫將來model的VO預作準備)
+						InputStream in = part.getInputStream();
+						photo = new byte[in.available()];						
+						in.close();						
+					}
+				}
+				
+				
+				
+				
+				String groupType = req.getParameter("groupType");
+				
+				
+				Integer totalAmout	=0;
+				
+			
+				java.sql.Date startTime = null;
+//				try {
+				String startTimes =null;
+				SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+			
+			
+				if("1".equals(req.getParameter("orderT"))) {
+					startTimes=req.getParameter("startTime");
+				}
+				else {
+				    startTimes=req.getParameter("startTimes");
+				}			
+				
+				startTime = new Date(simpleDateFormat.parse(startTimes).getTime());
+//				} catch (IllegalArgumentException e) {
+//					startTime=new java.sql.Date(System.currentTimeMillis());
+//					errorMsgs.add("請輸入日期!");
+//				}
+				
+				
+				Integer rate =5;
+				
+				
+				String note =req.getParameter("note");
+				String notes = "^[(\u4e00-\u9fa5)(a-zA-Z0-9_)]{1,20}$";
+				if (note == null || note.trim().length() == 0) {
+					errorMsgs.add("員工姓名: 請勿空白");
+				} else if(!note.trim().matches(notes)) { //以下練習正則(規)表示式(regular-expression)
+					errorMsgs.add("員工姓名: 只能是中、英文字母、數字和_ , 且長度必需在2到10之間");
+	            }
+				
+				
+				
+				GroupBandVO groupBandVO = new GroupBandVO();
+				groupBandVO.setContent(content);
+				groupBandVO.setIntroduction(introduction);
+				groupBandVO.setGroupStatus(groupStatus);
+				groupBandVO.setCurrenTnum(currenTnum);
+				groupBandVO.setUpperLimit(upperLimit);
+				groupBandVO.setLowerLimit(lowerLimit);
+				groupBandVO.setGroupName(groupName);
+				groupBandVO.setGroupLeader(groupLeader);
+				groupBandVO.setStartLoc(startLoc);
+				groupBandVO.setEndLoc(endLoc);
+				groupBandVO.setPrivates(privates);
+				groupBandVO.setPhoto(photo);
+				groupBandVO.setGroupType(groupType);
+				groupBandVO.setTotalAmout(totalAmout);
+
+				groupBandVO.setStartTime(startTime);
+//				groupBandVO.setStartTime(new Date(simpleDateFormat.parse("2019-02-14").getTime()));
+				groupBandVO.setRate(rate);
+				groupBandVO.setNote(note);
+			
+				
+
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					req.setAttribute("GroupBandVO", groupBandVO); // �t����J�榡���~��GroupBandVO����,�]�s�Jreq
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/front-end/groupBand/updateGroupBand.jsp");
+					failureView.forward(req, res);
+					return;
+				}
+				
+				/***************************2.�}�l�s�W���***************************************/
+				GroupBandService groupBandService = new GroupBandService();
+				groupBandVO = groupBandService.addGroupBand(content,introduction,groupStatus,currenTnum,
+						upperLimit,lowerLimit,groupName,groupLeader,startLoc,
+						endLoc,privates,photo,groupType,totalAmout,startTime,
+						rate,note);
+				
+
+				
+				
+				/***************************3.�s�W����,�ǳ����(Send the Success view)***********/
+//				req.setAttribute("GroupBandVO", groupBandVO);
+				String url = "/front-end/groupBand/listAllGroupBand.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url); // �s�W���\�����listAllEmp.jsp
+				successView.forward(req, res);				
+				
+				/***************************��L�i�઺���~�B�z**********************************/
+			} catch (Exception e) {
+				errorMsgs.add(e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/front-end/groupBand/updateGroupBand.jsp");
+				failureView.forward(req, res);
+			}		
+	   		
+	   		
+	   		
+	   	}
 	      if ("insert".equals(action)) { // �Ӧ�addEmp.jsp���ШD  
 				
 				List<String> errorMsgs = new LinkedList<String>();
