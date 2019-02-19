7567b7339e892679c48e55b78f542e9ade5abb44
Sam <Sam@LAPTOP-8SIETBN0>
2019-02-20 02:10:54 +0800

    增加getall方法

--- src/com/member/controller/MemberServlet.java
+++ src/com/member/controller/MemberServlet.java
@@ -3,12 +3,12 @@ package com.member.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.RequestDispatcher;
 
 import com.member.model.*;
 
@@ -20,31 +20,46 @@ public class MemberServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		
+		doPost(req, res);
 	}
 
-	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		
+		req.setCharacterEncoding("UTF-8");
 		res.setContentType("text/html;charset=UTF-8");
 		String action = req.getParameter("action");
-		PrintWriter out = res.getWriter();
-		
-		MemberDAO memberDAO = new MemberDAO();
-		MemberVO memberVO = memberDAO.findByPrimaryKey(action);
-		System.out.println(memberVO.getCreditcard());
-		out.println(memberVO.getName());
-		
-		List<MemberVO> list = memberDAO.getAll();
-		for(MemberVO memberVO2 : list) {
-			out.println(memberVO2.getName());
-			out.println(memberVO2.getActivityToken()); 
+//		PrintWriter out = res.getWriter();
+		if ("getAll".equals(action)) {
+			
+			// 開始查詢資料
+			MemberDAO memberDAO = new MemberDAO();
+			List<MemberVO> list = memberDAO.getAll();
+			
+			HttpSession session = req.getSession();
+			session.setAttribute("list", list);
+			
+			String url = "/member/listAllMember_getFormSession.jsp";
+			RequestDispatcher successView = req.getRequestDispatcher(url);
+			successView.forward(req, res);
+			return;
 		}
+		
+//		MemberDAO memberDAO = new MemberDAO();
+//		MemberVO memberVO = memberDAO.findByPrimaryKey(action);
+//		System.out.println(memberVO.getCreditcard());
+//		out.println(memberVO.getName());
+		
+//		List<MemberVO> list = memberDAO.getAll();
+//		for(MemberVO memberVO2 : list) {
+//			out.println(memberVO2.getName());
+//			out.println(memberVO2.getActivityToken()); 
+//		}
 		
 		
 		
 //		memberDAO.delete("ation");
 //		out.print("delete ok");
-		doGet(req, res);
+		
 	}
 
 }
