0eb3c44ad4f661d01af4c26cf7aa9b44a69e5a04
weichen <weichen@DESKTOP-K4BTSJF>
2019-02-22 10:28:34 +0800

    解決衝突

--- src/com/activity/model/ActivityService.java
+++ src/com/activity/model/ActivityService.java
@@ -8,14 +8,14 @@ public class ActivityService {
 	
 	/*創建本類別時，即創建一個DAO實體 用父介面型態的實體變數保存，目的是讓本類別中的所有方法可以用此DAO實體 以父介面的方法操作VO*/
 	public ActivityService(){
-		dao=new ActivityJDBCDAO();
+		dao=new ActivityJNDIDAO();
 	}
 	
 	/*相較於ActivityJDBCDAO中的Insert()，根據SQL常數，用vo.getxxx 去set pstmt需要的值*/
 	/*此處寫一個方法，直接將 vo變數值 傳入參數，用vo. setxxx予以保存*/
-	public ActivityVO addActivity(String activity_Id,String activity_Name,String activity_Info,Date activity_Start,Date activity_End,String activity_Code,Integer token_Amount,byte[]activity_Post){
+	public ActivityVO addActivity(String activity_Name,String activity_Info,Date activity_Start,Date activity_End,String activity_Code,Integer token_Amount,byte[]activity_Post){
 		ActivityVO activityVO=new ActivityVO();
-		activityVO.setActivityID(activity_Id);
+		
 		activityVO.setActivityName(activity_Name);
 		activityVO.setActivityInfo(activity_Info);
 		activityVO.setActivityStart(activity_Start);
