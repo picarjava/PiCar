b004883e537d3a2e85a9e3233cbe72ebf5ba997c
Sam <Sam@LAPTOP-8SIETBN0>
2019-02-25 01:24:12 +0800

    刪除方法

--- src/com/rate/model/RateDAO_interface.java
+++ src/com/rate/model/RateDAO_interface.java
@@ -9,7 +9,5 @@ public interface RateDAO_interface {
 	public void delete(Integer rateID);	
 	public RateVO findByPrimaryKey(Integer rateID);
 	public List<RateVO> getAll();
-	public RateVO findPic(Integer rateID);
-	public void insertPic(RateVO rateVO);
 
 }
