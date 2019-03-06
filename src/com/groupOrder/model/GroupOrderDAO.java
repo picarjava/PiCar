346a0cd27b923440eae4bffde2f7b255aad603fe
jykuo <kiek945@gmail.com>
2019-03-06 15:07:50 +0800

    +++

--- src/com/groupOrder/model/GroupOrderDAO.java
+++ src/com/groupOrder/model/GroupOrderDAO.java
@@ -32,6 +32,8 @@ public class GroupOrderDAO implements GroupOrderDAO_interface {
 			"SELECT GORDER_ID ,DRIVER_ID, MEM_ID,STATE,TOTAL_AMOUT,LAUNCH_TIME,START_TIME,END_TIME,START_LNG,START_LAT,END_LNG,END_LAT,ORDER_TYPE,RATE,NOTE,GROUP_ID FROM GROUP_ORDER";
 		private static final String GET_ONE_STMT = 
 			"SELECT GORDER_ID ,DRIVER_ID, MEM_ID,STATE,TOTAL_AMOUT,LAUNCH_TIME,START_TIME,END_TIME,START_LNG,START_LAT,END_LNG,END_LAT,ORDER_TYPE,RATE,NOTE,GROUP_ID FROM GROUP_ORDER where GORDER_ID = ?";
+		private static final String GET_ALL_MemID = 
+				"SELECT GORDER_ID ,DRIVER_ID, MEM_ID,STATE,TOTAL_AMOUT,LAUNCH_TIME,START_TIME,END_TIME,START_LNG,START_LAT,END_LNG,END_LAT,ORDER_TYPE,RATE,NOTE,GROUP_ID FROM GROUP_ORDER where MEM_ID = ?";
 		private static final String DELETE = 
 			"DELETE FROM GROUP_ORDER where GORDER_ID = ?";
 		private static final String UPDATE = 
@@ -197,6 +199,68 @@ public class GroupOrderDAO implements GroupOrderDAO_interface {
 			groupOrderVO.setGroupID(rs.getString("GROUP_ID"));
 			
 		}
+		}catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		return groupOrderVO;
+	}
+		public GroupOrderVO findByALLMemID(String groupOrderno) {
+			// TODO Auto-generated method stub
+			GroupOrderVO groupOrderVO =null;
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			try{
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(GET_ALL_MemID);
+			pstmt.setString(1, groupOrderno);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				
+			
+				groupOrderVO = new GroupOrderVO();
+				groupOrderVO.setGorderID(rs.getString("GORDER_ID"));
+				groupOrderVO.setDriverID(rs.getString("DRIVER_ID"));
+				groupOrderVO.setMemID(rs.getString("MEM_ID"));
+				groupOrderVO.setState(rs.getInt("STATE"));
+				groupOrderVO.setTotalAmout(rs.getInt("TOTAL_AMOUT"));
+				groupOrderVO.setLaunchTime(rs.getTimestamp("LAUNCH_TIME"));
+				groupOrderVO.setStartTime(rs.getTimestamp("START_TIME"));
+				groupOrderVO.setEndTime(rs.getTimestamp("END_TIME"));
+				groupOrderVO.setStartLng(rs.getDouble("START_LNG"));
+				groupOrderVO.setStartLat(rs.getDouble("START_LAT"));
+				groupOrderVO.setEndLng(rs.getDouble("END_LNG"));
+				groupOrderVO.setEndLat(rs.getDouble("END_LAT"));
+				groupOrderVO.setOrderType(rs.getInt("ORDER_TYPE"));
+				groupOrderVO.setRate(rs.getInt("RATE"));
+				groupOrderVO.setNote(rs.getString("NOTE"));
+				groupOrderVO.setGroupID(rs.getString("GROUP_ID"));
+				
+			}
 		}catch (SQLException se) {
 			throw new RuntimeException("A database error occured. "
 					+ se.getMessage());
