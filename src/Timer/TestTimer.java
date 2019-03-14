ff44f9cfbb8b78d3c19f34d2a1740984faf2fb37
Sam <samchiang74@gmail.com>
2019-03-14 22:51:50 +0800

    測試TIMER

---
+++ src/Timer/TestTimer.java
@@ -0,0 +1,35 @@
+package Timer;
+
+public class TestTimer {
+	ServletTimer2 st2 = new ServletTimer2();
+	
+	
+
+	public static void main(String[] args) {
+		
+		
+		ServletTimer2 st2 = new ServletTimer2();
+		st2.doTimeCancle("a");
+		st2.doTimeCancle("b");
+		
+		
+		
+		
+		
+		
+//		st2.dotime("a司機");
+//		
+//		st2.cancel();
+//		st2.dotime("b司機");
+//		
+//		st2.dotime("c司機");
+//		st2.dotime("d司機");
+//		st2.dotime("e司機");
+//		st2.dotime("f司機");
+//		
+//		
+//		st2.cancel();
+	
+	}
+
+}
