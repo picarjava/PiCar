89384252342ad123b5fa0a5b2ce5c122d9c3ce96
jyun <jyunliou120@hotmail.com>
2019-03-27 12:40:23 +0800

    修改mailservice帳號

--- src/com/mailService/MailService.java
+++ src/com/mailService/MailService.java
@@ -27,8 +27,8 @@ public class MailService {
 
        // ●設定 gmail 的帳號 & 密碼 (將藉由你的Gmail來傳送Email)
        // ●須將myGmail的【安全性較低的應用程式存取權】打開
-	     final String myGmail = "jyunj5674@gmail.com";
-	     final String myGmail_password = "QqAa123321";
+	     final String myGmail = "2019picar.inc@gmail.com";
+	     final String myGmail_password = "picar20190329";
 		   Session session = Session.getInstance(props, new Authenticator() {
 			   protected PasswordAuthentication getPasswordAuthentication() {
 				   return new PasswordAuthentication(myGmail, myGmail_password);
